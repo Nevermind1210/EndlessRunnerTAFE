8ec223fc47bb12f87419ee26e8d3b80a77419686
Xavier Arias <xavier_rate@outlook.com>
2021-02-26 14:31:28 +1100

    A overhaul of PlayerMovement to Sidescroller

--- Assets/Endless Runner/Scripts/PlayerController.cs
+++ Assets/Endless Runner/Scripts/PlayerController.cs
@@ -2,32 +2,101 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
-    private Vector2 targetPos;
-    public float Yincrement;
-    public float speed;
-    public float maxHeight;
-    public float minHeight;
+    //public Image staminaBar;
+    private Rigidbody2D rb;
+    private float move;
 
-    public int pHealth = 3;
+    [SerializeField] public float moveSpeed = 10;          //Var 1
+    [SerializeField] public float jumpHeight = 5;          //Var 2   
+    [SerializeField] public bool grounded;
+    [SerializeField] public float sprintMulti = 2;         //Var 3
+    [SerializeField] public float pHealth = 5;             //Var 4
+                     
+    [SerializeField] public float maxFuel = 5;             //Var 5
+    [SerializeField] public float currentFuel = 5;
+    [SerializeField] public float jetpackForce = 10;       //Var 6
+                     
+    [SerializeField] public GameObject flame;
+                     
+                     
+    [SerializeField] public float maxStamina = 10;          //Var 7
+    [SerializeField] public float currentStamina = 10;
 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        flame.SetActive(false);
+    }
+
+    // Update is called once per frame
     void Update()
     {
+        move = Input.GetAxisRaw("Horizontal") * moveSpeed;
+
         if (pHealth <= 0) // Game Over.
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
 
-        transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.LeftShift) && currentStamina > 0)
+        {
+            move = move * sprintMulti;
+            currentStamina -= 2 * Time.deltaTime;
+        }
+        else if (!Input.GetKey(KeyCode.LeftShift) && currentStamina < maxStamina)
+        {
+            currentStamina += 1 * Time.deltaTime;
+        }
+        //staminaBar.fillAmount = currentStamina / maxStamina; 
 
-        if (Input.GetKeyDown(KeyCode.UpArrow) && transform.position.y < maxHeight)
+
+        if (Input.GetButtonDown("Jump") && grounded)
         {
-            targetPos = new Vector2(transform.position.x, transform.position.y + Yincrement);
-        } else if (Input.GetKeyDown(KeyCode.DownArrow) && transform.position.y > minHeight)
+            rb.velocity += Vector2.up * jumpHeight;
+        }
+        else if (Input.GetButton("Jump") && grounded == false)
         {
-            targetPos = new Vector2(transform.position.x, transform.position.y - Yincrement);
+            if (currentFuel > 0)
+            {
+                flame.SetActive(true);
+                rb.velocity += Vector2.up * jetpackForce * Time.deltaTime;
+                currentFuel -= Time.deltaTime;
+            }
+        }
+        else if (Input.GetButtonUp("Jump"))
+        {
+            flame.SetActive(false);
+        }
+
+
+    }
+
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(move, rb.velocity.y);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            grounded = true;
+            currentFuel = maxFuel;
         }
     }
+
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        grounded = false;
+    }
+
+
 }
