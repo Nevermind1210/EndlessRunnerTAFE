33f174af3dfa8d40ed78cf4abd19bd0e0efa8148
Noahsonicx <53206519+Noahsonicx@users.noreply.github.com>
2021-03-05 15:21:42 +1100

    Update LevelGeneration.cs

--- Assets/Endless Runner/Scripts/LevelGeneration.cs
+++ Assets/Endless Runner/Scripts/LevelGeneration.cs
@@ -2,8 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class LevelGeneration : MonoBehaviour
 {
+    public float obsticleCount;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,13 @@ public class LevelGeneration : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if obsticleCount = 0
+        {
+            then skip obsticleCount;
+        }
+        else
+        {
+            obsticleCount + 1;
+        }
     }
 }
