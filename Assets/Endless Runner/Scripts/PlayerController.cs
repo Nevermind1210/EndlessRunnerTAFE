94a58bb6a865caf1975e5105b0c2dff7a820536e
NegativeVibes <xavier_rate@outlook.com>
2021-03-26 01:12:50 +1100

    Added Main Menu & Game Over
    They are incredibly simplistic, game over is there but not functionally going to work I will get on working on it tomorrow but the game itself is not done final hours...

--- Assets/Endless Runner/Scripts/PlayerController.cs
+++ Assets/Endless Runner/Scripts/PlayerController.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(BoxCollider2D))]
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private LayerMask platLayerMask;
