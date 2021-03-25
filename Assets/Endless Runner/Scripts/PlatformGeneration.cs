9653aeefb18de1eec33fbfee7ce710a884ad7eda
Noahsonicx <53206519+Noahsonicx@users.noreply.github.com>
2021-03-25 12:58:02 +1100

    Changes to Generation.Platform
    Platform infinite looping generation

--- Assets/Endless Runner/Scripts/PlatformGeneration.cs
+++ Assets/Endless Runner/Scripts/PlatformGeneration.cs
@@ -9,22 +9,34 @@ public class PlatformGeneration : MonoBehaviour
     public float distanceBetween;
 
     private float platformWidth;
+    private Transform lastPlatform;
 
     // Start is called before the first frame update
     void Start()
     {
-        platformWidth = thePlatform.GetComponent<BoxCollider2D>().size.x;
+        platformWidth = thePlatform.transform.localScale.x;
+        distanceBetween = platformWidth;
+        lastPlatform = thePlatform.transform;
+
+        generationPoint.position = new Vector3(lastPlatform.position.x + distanceBetween, lastPlatform.position.y, lastPlatform.position.z);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(transform.position.x < generationPoint.position.x)
+        if(generationPoint.position.x - transform.position.x < distanceBetween)
         {
-            transform.position = new Vector3(transform.position.x + platformWidth + distanceBetween, transform.position.y, transform.position.z);
+            //transform.position = new Vector3(transform.position.x + platformWidth + distanceBetween, transform.position.y, transform.position.z);
 
-            Instantiate(thePlatform, transform.position, transform.rotation);
+            //Instantiate(thePlatform, transform.position, transform.rotation);
+
+            generationPoint.position = new Vector3(lastPlatform.position.x + distanceBetween, lastPlatform.position.y, lastPlatform.position.z);
+
+            lastPlatform = Instantiate(thePlatform, generationPoint.position, transform.rotation).transform;
         }
 
+        // If the position - generation point is less than some amount (threshold)
+            // Spawn platform
+            // Move generation point to position + threshold
     }
 }
